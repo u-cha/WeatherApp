4d16259e7f1bd5ddee663be0154f2dbd3d0c3cb9
Sergey Udachin <s.udachin@mail.ru>
2023-10-09 15:28:23 +0300

    Changed DB for PostgreSQL DB currently ran at localhost:5432

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -73,13 +73,24 @@ WSGI_APPLICATION = 'Weather.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weather_app_db',
+        'USER': 'weather_app_db_user',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
