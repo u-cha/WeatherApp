c9ff007fca7ccc77d28b3fb6a3e0dc767da42103
Sergey Udachin <s.udachin@mail.ru>
2023-10-09 16:01:59 +0300

    Fixed typo in settings.py

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'WeatherApp.apps.WeatherAppConfig'
+    'WeatherApp.apps.WeatherAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
