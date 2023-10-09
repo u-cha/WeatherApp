87e3bd9015b08aeff81871012de8bd6d84dc1289
Sergey Udachin <s.udachin@mail.ru>
2023-10-09 16:03:14 +0300

    Created Model Location, referencing Django model User

--- Weather/WeatherApp/models.py
+++ Weather/WeatherApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField()
+    users = models.ManyToManyField(User, related_name='locations')
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
