8172d788633fbdbc06febd41895fd7924c6aed06
Sergey Udachin <s.udachin@mail.ru>
2023-10-09 15:39:20 +0300

    Registered WeatherApp in settings.py

--- Weather/WeatherApp/apps.py
+++ Weather/WeatherApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WeatherappConfig(AppConfig):
+class WeatherAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'WeatherApp'
