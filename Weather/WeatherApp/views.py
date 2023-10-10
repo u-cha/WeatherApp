806ade2b064bd62aabc4568615e027f264111510
Sergey Udachin <s.udachin@mail.ru>
2023-10-10 11:30:30 +0300

    Create register view function, referencing Django UserCreationForm

--- Weather/WeatherApp/views.py
+++ Weather/WeatherApp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello from WeatherApp")
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm()
+    return render(request, template_name="WeatherApp/register.html", context={"form": form})
