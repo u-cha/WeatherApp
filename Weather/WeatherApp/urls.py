8f10f0e152118ed11cce2da210ebda97c922c8ed
Sergey Udachin <s.udachin@mail.ru>
2023-10-10 10:04:36 +0300

    Added login/ path in WeatherApp urls.py leading to LoginView on login.html template

--- Weather/WeatherApp/urls.py
+++ Weather/WeatherApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from .import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", LoginView.as_view(template_name="WeatherApp/login.html")),
 ]
