8c9ade46e99001045b5f68bb5254b6f5ddd907c0
Sergey Udachin <s.udachin@mail.ru>
2023-10-10 11:52:48 +0300

    Define register view function

--- Weather/WeatherApp/views.py
+++ Weather/WeatherApp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
@@ -11,7 +11,14 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm()
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
     else:
         form = UserCreationForm()
     return render(request, template_name="WeatherApp/register.html", context={"form": form})
