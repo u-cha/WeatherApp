dd064b40c1735f6802d17ff928f9312694cb45ba
Sergey Udachin <s.udachin@mail.ru>
2023-10-10 11:26:41 +0300

    Add register/ path leading to signup view function

--- Weather/WeatherApp/urls.py
+++ Weather/WeatherApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from . import views
 
 from .import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", LoginView.as_view(template_name="WeatherApp/login.html", next_page="index"), name="login"),
+    path("register/", views.signup, name="register"),
 ]
