bde0386a5c54a89f1715f4fe612bfc8f8256f3f5
Sergey Udachin <s.udachin@mail.ru>
2023-10-12 17:01:48 +0300

    Split LocationHiddenForm into AddForm and DeleteForm

--- Weather/WeatherApp/forms.py
+++ Weather/WeatherApp/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
 
-class LocationHiddenForm(forms.Form):
+class LocationAddHiddenForm(forms.Form):
     name = forms.CharField(widget=forms.HiddenInput)
     latitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
     longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
     country = forms.CharField(widget=forms.HiddenInput)
+
+
+class LocationDeleteHiddenForm(forms.Form):
+    latitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
+    longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
+
