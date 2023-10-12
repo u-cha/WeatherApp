288ad66e418ff6291124712345ed72bdfb555dc3
Sergey Udachin <s.udachin@mail.ru>
2023-10-12 13:05:19 +0300

    Add profile url in urlpatterns

--- Weather/WeatherApp/urls.py
+++ Weather/WeatherApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="WeatherApp/login.html", next_page="index"), name="login"),
     path("register/", views.register, name="register"),
     path("lookup/<str:location_name>", views.location_lookup, name='location_lookup'),
+    path("profile/", views.profile, name="profile"),
 ]
