a229d4a0b2fad03dcda941f79ac98d0631d72eb7
Sergey Udachin <s.udachin@mail.ru>
2023-10-15 17:12:14 +0300

    "Silly update to check git work"

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validatorssth
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- Weather/WeatherApp/forms.py
+++ Weather/WeatherApp/forms.py
@@ -11,4 +11,3 @@ class LocationAddHiddenForm(forms.Form):
 class LocationDeleteHiddenForm(forms.Form):
     latitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
     longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=7, decimal_places=4)
-
