7fe1897c2708a34db1015d83882abfb0b7d250a6
Sergey Udachin <s.udachin@mail.ru>
2023-10-16 13:14:38 +0300

    Add model Weather

--- Weather/WeatherApp/models.py
+++ Weather/WeatherApp/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+import json
 
 
-# Create your models here.
+class Weather:
+    def __init__(self, short_type: str, temperature: float, description: str):
+        self.short_type = short_type
+        self.temperature = temperature
+        self.description = description
+
+    @staticmethod
+    def from_json(weather_json):
+        weather_dict = weather_json
+        try:
+            short_type = weather_dict["weather"][0]["main"]
+            temperature = weather_dict["main"]["temp"]
+            description = weather_dict["weather"][0]["description"]
+        except KeyError:
+            return False
+        return Weather(short_type=short_type, temperature=temperature, description=description)
+
+
 class Location(models.Model):
     name = models.CharField()
     users = models.ManyToManyField(User, related_name='locations')
