a47b4278265a324057fbba47c9b192f777bbf871
Sergey Udachin <s.udachin@mail.ru>
2023-10-16 14:37:35 +0300

    Clean mess

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -72,17 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Weather.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
