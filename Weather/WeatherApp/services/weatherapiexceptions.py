c12a92c1ceea1f0c1773e8ae55fbd4263a3f9c17
Sergey Udachin <s.udachin@mail.ru>
2023-10-16 13:15:31 +0300

    Fix exception definitions

--- Weather/WeatherApp/services/weatherapiexceptions.py
+++ Weather/WeatherApp/services/weatherapiexceptions.py
@@ -4,20 +4,25 @@ class WeatherAPIError(Exception):
 
 
 class WeatherAPIFailure(WeatherAPIError):
-    super().__init__(message="There was a problem on remote API side")
+    def __init__(self, message="There was a problem on remote API side"):
+        self.message = message
 
 
 class WeatherAPIRequestError(WeatherAPIError):
-    super().__init__(message="There is an issue with your request. Please consult remote API docs")
+    def __init__(self, message="There is an issue with your request. Please consult remote API docs"):
+        self.message = message
 
 
 class WeatherAPIKeyError(WeatherAPIError):
-    super().__init__(message="There is an issue with your API key")
+    def __init__(self, message="There is an issue with your API key"):
+        self.message = message
 
 
 class WeatherAPISubscriptionError(WeatherAPIError):
-    super().__init__(message="There is subscription error. Please consult remote API terms and conditions")
+    def __init__(self, message="There is subscription error. Please consult remote API terms and conditions"):
+        self.message = message
 
 
 class WeatherAPIUnknownError(WeatherAPIError):
-    super().__init__(message="There was a problem on remote API side")
+    def __init__(self, message="There was a problem on remote API side"):
+        self.message = message
