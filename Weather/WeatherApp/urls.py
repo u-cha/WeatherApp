7bcf82123a3897b18a67d3b855c3bf94a41378d5
Sergey Udachin <s.udachin@mail.ru>
2023-10-16 14:40:26 +0300

    Refactor route sequence

--- Weather/WeatherApp/urls.py
+++ Weather/WeatherApp/urls.py
@@ -4,10 +4,10 @@ from .import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", LoginView.as_view(template_name="WeatherApp/login.html", next_page="index"), name="login"),
     path("register/", views.register, name="register"),
-    path("location/", views.location_lookup, name='location_lookup'),
-    path("profile/", views.profile, name="profile"),
+    path("login/", LoginView.as_view(template_name="WeatherApp/login.html", next_page="index"), name="login"),
     path("logout/", views.log_out, name="logout"),
+    path("profile/", views.profile, name="profile"),
+    path("location/search/", views.location_search, name='location_lookup'),
     path("location/delete/", views.location_delete, name="location_delete"),
 ]
