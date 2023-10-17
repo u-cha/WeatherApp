96070fec1560dd19d014d78d193d0745446b4649
Sergey Udachin <s.udachin@mail.ru>
2023-10-17 10:15:35 +0300

    Remove-d Location form duplication

--- Weather/WeatherApp/forms.py
+++ Weather/WeatherApp/forms.py
@@ -1,15 +1,10 @@
 from django import forms
 
 
-class LocationAddHiddenForm(forms.Form):
+class LocationHiddenForm(forms.Form):
     name = forms.CharField(widget=forms.HiddenInput)
     latitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=23, decimal_places=20)
     longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=23, decimal_places=20)
     country = forms.CharField(widget=forms.HiddenInput)
 
 
-class LocationCoordinatesHiddenForm(forms.Form):
-    name = forms.CharField(widget=forms.HiddenInput)
-    latitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=23, decimal_places=20)
-    longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=23, decimal_places=20)
-    country = forms.CharField(widget=forms.HiddenInput)
