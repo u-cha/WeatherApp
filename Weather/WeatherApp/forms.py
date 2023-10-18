37527afef05a40f01552284a7d84f31a0de80f06
Sergey Udachin <s.udachin@mail.ru>
2023-10-18 17:20:51 +0300

    Add convenience method to_dict

--- Weather/WeatherApp/forms.py
+++ Weather/WeatherApp/forms.py
@@ -7,4 +7,14 @@ class LocationHiddenForm(forms.Form):
     longitude = forms.DecimalField(widget=forms.HiddenInput, max_digits=23, decimal_places=20)
     country = forms.CharField(widget=forms.HiddenInput)
 
+    def to_dict(self):
+        return {
+            "name": self.data["name"],
+            "latitude": self.data["latitude"],
+            "longitude": self.data["longitude"],
+            "country": self.data["country"]
+        }
+
+
+
 
