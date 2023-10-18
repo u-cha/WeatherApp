5515eb5b36f29981d20b4306ee74721b1109bb27
Sergey Udachin <s.udachin@mail.ru>
2023-10-18 17:26:27 +0300

    Refactor rename weather_show view function

--- Weather/WeatherApp/urls.py
+++ Weather/WeatherApp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("location/search/", views.location_search, name='location_lookup'),
     path("location/delete/", views.location_delete, name="location_delete"),
     path("location/add/", views.location_add, name="location_add"),
-    path("weather/show/", views.weather_lookup, name="weather_lookup"),
+    path("weather/show/", views.weather_show, name="weather_show"),
 ]
